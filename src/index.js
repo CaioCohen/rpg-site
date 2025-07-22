6f02ca046ad2eb3f5040c4f8622c32aa09543a0a
Caio Cohen <caiocds1@gmail.com>
2025-07-22 06:37:32 -0300

    adicionando basename

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DataProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/rpg-site">
         <App />
       </BrowserRouter>
     </DataProvider>
